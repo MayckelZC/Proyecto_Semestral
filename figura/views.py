c02b663e70fb5bd8601fce488da067e166f5ec4b
Yeremi Guerrero <131369265+YeremG@users.noreply.github.com>
2023-06-25 02:22:26 -0400

    Se agrega validacion de registro

--- figura/views.py
+++ figura/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect , get_object_or_404
 from .forms import ContactoForm, FigurasForm, CustomUserCreationForm
 from .models import Carrusel , Figuras 
 from django.contrib import messages
-from django.contrib
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -73,10 +73,13 @@ def registro(request):
         'form':CustomUserCreationForm()
     }
 
-    if request.method == 'POST'
-        formulario = CustomUserCreationForm(context=request.POST)
+    if request.method == 'POST':
+        formulario = CustomUserCreationForm(data=request.POST)
         if formulario.is_valid():
             formulario.save()
+            user = authenticate(username=formulario.cleaned_data["username"], password=formulario.cleaned_data["password1"])
+            login(request, user)
+            messages.success(request,"Registrado con Exito")           
             return redirect(to=index)
         context["form"] = formulario
     return render(request, 'registration/registrar.html',context)
