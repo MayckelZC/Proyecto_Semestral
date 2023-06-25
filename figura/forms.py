370fc50c1c2ea906e858211e6873541b814b587e
Yeremi Guerrero <131369265+YeremG@users.noreply.github.com>
2023-06-25 02:22:53 -0400

    Se agregan campos a registro

--- figura/forms.py
+++ figura/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-
 from figura.models import Contacto, Figuras
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactoForm(forms.ModelForm):
 
@@ -15,4 +15,7 @@ class FigurasForm(forms.ModelForm):
         model = Figuras
         fields='__all__'
 class CustomUserCreationForm(UserCreationForm):
-    pass
+    
+    class Meta:
+        model = User
+        fields=['username', "first_name","email","password1","password2"]
