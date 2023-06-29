dfadd44262b1079a97e0ccb07286ff0b787d7953
Mayckel Zepeda <mayckelzc@gmail.com>
2023-06-29 06:13:26 -0400

    FINAL

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Mayckel\Documents\GitHub\Proyecto_Semestral\myvenv
+set VIRTUAL_ENV=C:\Users\ellol\Desktop\Proyecto_Semestral-main\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
